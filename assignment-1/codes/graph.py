6d3f4a6941796b478a2ec1c730e76a0d66dce7eb
prabhat.kukunuri <prabhat.kukunuri@gmail.com>
2024-01-04 14:52:42 +0530

    revised

--- assignment-1/codes/graph.py
+++ assignment-1/codes/graph.py
@@ -1,11 +1,11 @@
 # importing libraries
-import numpy as nu
-import matplotlib.pyplot as plo
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-x = nu.arange(0, 0.00000005, 0.000000000001)
-y = nu.cos((2/nu.pi)*(10**9)*x)
-plo.plot(x, y, color='green')
-plo.show()
+x = np.arange(0, 0.00000005, 0.000000000001)
+y = np.cos((2/nu.pi)*(10**9)*x)
+plt.plot(x, y, color='green')
+plt.show()
 
 
