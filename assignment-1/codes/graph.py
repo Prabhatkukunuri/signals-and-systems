7c99d397162f4914acb3e8bdcc7e7bf54accaf53
prabhat.kukunuri <prabhat.kukunuri@gmail.com>
2024-01-05 20:27:00 +0530

    rectified the repository

--- assignment-1/codes/graph.py
+++ assignment-1/codes/graph.py
@@ -1,11 +1,16 @@
-# importing libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Generate time values
+t = np.linspace(0, 1e-6, 1000)  # Time values from 0 to 1e-6 seconds with 1000 points
 
-x = np.arange(0, 0.00000005, 0.000000000001)
-y = np.cos((2/nu.pi)*(10**9)*x)
-plt.plot(x, y, color='green')
+# Calculate the corresponding y values
+y = np.cos(2 * np.pi * 1e9 * t)
+
+# Plot the graph
+plt.plot(t, y)
+plt.title('Graph of y = cos(2*pi*(10^9)*t)')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude of electromagnetic wave')
+plt.grid(True)
 plt.show()
-
-
